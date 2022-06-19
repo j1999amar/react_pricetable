7f7405a3083d9335d4ac63f1df11a7c6e877c107
j1999amar <j1999amar@gmail.com>
2022-06-19 20:16:25 +0530

    text center

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar(){
     return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow d-flex justify-content-between">
-      <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow d-flex justify-content-between text-center">
+    <h1 class="navbar-brand">Company name</h1>
       <nav className="my-2 my-md-0 mr-md-3 navlist  ">
         <a href="#" className="p-2 text-dark text-decoration-none " >Features</a>
         <a href="#" className="p-2 text-dark text-decoration-none" >Enterprise</a>
